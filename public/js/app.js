823c67d9b757b36ec9f731328c9d33cf46d36494
Yogesh Mahale <bonzer.yogesh@gmail.com>
2020-08-30 19:19:03 +0530

    Update app js

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,6 @@
-var app = angular.module('imdbApp', ['ngResource', 'ui.router', 'ngSanitize', 'ui.bootstrap',  'ngMessages', 'ngRoute']);
+var app = angular.module('imdbApp', ['ngCookies', 'ngResource', 'ui.router', 'ngSanitize', 'ui.bootstrap',  'ngMessages', 'ngRoute', 'ngAnimate', 'isteven-multi-select',
+    'ui-notification', 'imdbApp.system', 'imdbApp.auth', 'imdbApp.user']);
 
-
-
-
-
-/*var app = angular.module('zogataApp', ['ngCookies', 'ui.mention', 'ngResource', 'ui.router', 'ui.tinymce', 'ui.tiny', 'ngFileUpload', 'ngAnimate', 'ngTagsInput','ngDragDrop', 'dragularModule', 'ui-notification', 'ngSanitize', 'ngAria','moment-picker', 'ngMessages', 'ui.bootstrap', 'ngBootstrap', 'ui.route', 'multi-auto-complete', 'ngScrollbars', 'zogataApp.system', 'zogataApp.auth', 'zogataApp.notification', 'zogataApp.reminder', 'zogataApp.notebook', 'zogataApp.chat', 'zogataApp.contact', 'zogataApp.opportunity', 'zogataApp.task','zogataApp.company', 'zogataApp.tickets', 'chart.js', 'angularjs-gauge', 'btford.socket-io', 'isteven-multi-select', 'zogataApp.webSurvey', 'zogataApp.emailSurvey', 'zogataApp.npsSurvey', 'zogataApp.cesSurvey', 'zogataApp.csatSurvey', 'zogataApp.survey', 'zogataApp.workflow', 'zogataApp.ecommerce', 'zogataApp.funnel', 'mentio','angularMoment']);*/
+angular.module('imdbApp.system', []);
+angular.module('imdbApp.auth', []);
+angular.module('imdbApp.user', []);
